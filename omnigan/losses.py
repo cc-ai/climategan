a1b8e5912c8bb3106c796dbb4d2fa078df909da6
vict0rsch <vsch@pm.me>
2019-11-29 16:42:55 -0500

    travelloss

--- omnigan/losses.py
+++ omnigan/losses.py
@@ -32,3 +32,30 @@ class GANLoss(nn.Module):
 def cross_entropy():
     # loss(logits, target) with target as classes, NOT 1-h encoded
     return torch.nn.CrossEntropyLoss()
+
+
+class TravelLoss(nn.Module):
+    def __init__(self, eps=1e-12):
+        super().__init__()
+        self.eps = eps
+
+    def cosine_loss(self, real, fake):
+        norm_real = torch.norm(real, p=2, dim=1)[:, None]
+        norm_fake = torch.norm(fake, p=2, dim=1)[:, None]
+        mat_real = real / norm_real
+        mat_fake = fake / norm_fake
+        mat_real = torch.max(mat_real, self.eps * torch.ones_like(mat_real))
+        mat_fake = torch.max(mat_fake, self.eps * torch.ones_like(mat_fake))
+        # compute only the diagonal of the matrix multiplication
+        return torch.einsum("ij, ji -> i", mat_fake, mat_real).sum()
+
+    def __call__(self, S_real, S_fake):
+        self.v_real = []
+        self.v_fake = []
+        for i in range(len(S_real)):
+            for j in range(i):
+                self.v_real.append((S_real[i] - S_real[j])[None, :])
+                self.v_fake.append((S_fake[i] - S_fake[j])[None, :])
+        self.v_real_t = torch.cat(self.v_real, dim=0)
+        self.v_fake_t = torch.cat(self.v_fake, dim=0)
+        return self.cosine_loss(self.v_real_t, self.v_fake_t)
