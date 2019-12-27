3c80e7443543ea9f54b908a464250274e54d32e5
vict0rsch <vsch@pm.me>
2019-12-27 19:22:26 +0100

    v0 upload to comet

---
+++ tests/test_comet.py
@@ -0,0 +1,51 @@
+import sys
+import comet_ml
+
+sys.path.append("..")
+
+from omnigan.trainer import Trainer
+from omnigan.utils import load_opts, get_comet_rest_api_key
+from run import print_header, bcolors
+
+if __name__ == "__main__":
+
+    rest_api_key = get_comet_rest_api_key()
+    comet_api = comet_ml.api.API()
+
+    opts = load_opts("../config/local_tests.yaml", default="../shared/defaults.yml")
+    trainer = Trainer(opts, comet=True, verbose=0)
+    trainer.setup()
+    multi_batch_tuple = next(iter(trainer.train_loaders))
+    domain_batch = {batch["domain"][0]: batch for batch in multi_batch_tuple}
+
+    print_header("test_log_losses")
+    trainer.update_g(domain_batch)
+    trainer.log_losses("G")
+    print("log")
+
+    trainer.logger.global_step += 1
+    trainer.update_g(domain_batch)
+    trainer.log_losses("G")
+    print("log")
+
+    trainer.logger.global_step += 1
+    trainer.update_g(domain_batch)
+    trainer.log_losses("G")
+    print("log")
+    trainer.exp.end()
+
+    should_delete = False
+    if should_delete:
+        comet_api.delete_experiment(trainer.exp.get_key())
+        print(
+            "{}Successfully deleted your comet exp. Edit should_delete not to{}".format(
+                bcolors.OKBLUE, bcolors.ENDC
+            )
+        )
+    else:
+        print(
+            "{}Your comet experiment was not deleted.".format(bcolors.WARNING),
+            "set should_delete to True not to pollute your workspace{}".format(
+                bcolors.ENDC
+            ),
+        )
