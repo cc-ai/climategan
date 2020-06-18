de15c3e47850b6db9fc16d54a08aa79a7d4f2384
Alexia Reynaud <r.alexia@hotmail.com>
2020-06-17 23:21:38 -0400

    Added test for NTXentLoss (works!)

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -7,9 +7,10 @@ import torch
 sys.path.append(str(Path(__file__).parent.parent.resolve()))
 from omnigan.data import get_all_loaders
 from omnigan.generator import get_gen
-from omnigan.losses import PixelCrossEntropy
+from omnigan.losses import PixelCrossEntropy, NTXentLoss
 from omnigan.utils import load_test_opts
 from run import print_header
+from omnigan.simclr import NetSimCLR
 
 
 parser = argparse.ArgumentParser()
@@ -40,6 +41,31 @@ if __name__ == "__main__":
     prediction = G.decoders["s"](z)
     pce = PixelCrossEntropy()
     print(pce(prediction.squeeze(), batch["data"]["s"].long().squeeze().to(device)))
-    # ! error how to infer from cropped data: input: 224 output: 256??
+    #! error how to infer from cropped data: input: 224 output: 256??
+
+    # -----------------------------------
+    # -------  Test NTXentLoss()  -------
+    # -----------------------------------
+    # For now, testing the loss on representations directly (eventually, test it on projections)
+    print_header("test_ntxent")
+
+    opts.gen.encoder.architecture = "base"
+    opts.gen.default.res_dim = 256
+    opts.gen.a.ignore = True
+
+    G = get_gen(opts).to(device)
+    z = G.encode(image)
+
+    latent_shape = z.shape[1:]
+    simclr_model = NetSimCLR(G.encoder, latent_shape, opts.gen.simclr.output_size)
+
+    image1 = torch.randn(opts.data.loaders.batch_size, 3, 32, 32).to(device)
+    image2 = torch.randn(opts.data.loaders.batch_size, 3, 32, 32).to(device)
+    h1, z1 = simclr_model.forward(image1)
+    h2, z2 = simclr_model.forward(image2)
+
+    print("Shape of projections:", z1.shape)
+    loss = NTXentLoss(opts.data.loaders.batch_size, 0.5, True)
+    print("NTXent loss:", loss(z1, z2).to(device))
 
     # TODO more test for the losses
