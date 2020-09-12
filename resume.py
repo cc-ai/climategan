849654fa36e04c7b41f517ee3d27630d0faa1c3f
vict0rsch <vsch@pm.me>
2020-09-11 20:03:01 -0400

    add resume utility

---
+++ resume.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+from time import time, sleep
+import os
+import re
+from argparse import ArgumentParser
+import numpy as np
+
+import yaml
+from addict import Dict
+from comet_ml import Experiment
+
+from omnigan.trainer import Trainer
+
+from omnigan.utils import env_to_path, flatten_opts, get_increased_path
+
+
+def latest_opts(path):
+    """
+    get latest opts dumped in path if they look like *opts*.yaml
+    and were increased as
+    opts.yaml < opts (1).yaml < opts (2).yaml etc.
+
+    Args:
+        path (str or pathlib.Path): where to look for opts
+
+    Raises:
+        ValueError: If no match for *opts*.yaml is found
+
+    Returns:
+        addict.Dict: loaded opts
+    """
+    all_opts = list(Path(path).resolve().glob("*opts*.yaml"))
+    if len(all_opts) == 0:
+        raise ValueError(
+            "Could not find any opts file as *opts*.yaml in {}".format(str(path))
+        )
+    if len(all_opts) == 1:
+        with all_opts[0].open("r") as f:
+            return Dict(yaml.safe_load(f))
+
+    ints = [int(re.findall("\((.*?)\)", fn)[0]) if "(" in fn else 0 for fn in all_opts]
+    amax = np.argmax(ints)
+    with all_opts[amax].open("r") as f:
+        return Dict(yaml.safe_load(f))
+
+
+def pprint(*args):
+    txt = " ".join(map(str, args))
+    col = "====="
+    space = "   "
+    head_size = 2
+    header = "\n".join(["=" * (len(txt) + 2 * (len(col) + len(space)))] * head_size)
+    empty = "{}{}{}{}{}".format(col, space, " " * (len(txt)), space, col)
+    print()
+    print(header)
+    print(empty)
+    print("{}{}{}{}{}".format(col, space, txt, space, col))
+    print(empty)
+    print(header)
+    print()
+
+
+def main():
+    """
+    Opts prevalence:
+        1. Load file specified in args.default (or shared/trainer/defaults.yaml
+           if none is provided)
+        2. Update with file specified in args.config (or no update if none is provided)
+        3. Update with parsed command-line arguments
+
+        e.g.
+        `python train.py args.config=config/large-lr.yaml data.loaders.batch_size=10`
+        loads defaults, overrides with values in large-lr.yaml and sets batch_size to 10
+    """
+
+    # -----------------------------
+    # -----  Parse arguments  -----
+    # -----------------------------
+    parser = ArgumentParser()
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--no_comet", action="store_true", default=False)
+    parser.add_argument("--note", type=str, default="")
+    parser.add_argument("--comet_tags", nargs="+", default=[])
+
+    args = parser.parse_args()
+    resume_path = Path(env_to_path(args.path)).resolve()
+    assert resume_path.exists()
+    # -----------------------
+    # -----  Load opts  -----
+    # -----------------------
+
+    opts = latest_opts(resume_path)
+    opts.train.resume = True
+    opts.output_path = str(env_to_path(opts.output_path))
+    pprint("Continuing model in", opts.output_path)
+
+    exp = None
+    # -------------------------------
+    # -----  Check output_path  -----
+    # -------------------------------
+
+    if not args.no_comet:
+        # ----------------------------------
+        # -----  Set Comet Experiment  -----
+        # ----------------------------------
+        exp = Experiment(project_name="omnigan", auto_metric_logging=False)
+        # log job id
+        opts.jobID = os.environ.get("SLURM_JOBID")
+        if args.note:
+            # log note
+            exp.log_parameter("note", args.note)
+        # merge and log tags from args and opts
+        if args.comet_tags or opts.comet.tags:
+            tags = set()
+            if args.comet_tags:
+                tags.update(args.comet_tags)
+            if opts.comet.tags:
+                tags.update(opts.comet.tags)
+            opts.comet.tags = list(tags)
+            exp.add_tags(opts.comet.tags)
+        # log all opts
+        exp.log_parameters(flatten_opts(opts))
+        sleep(1)
+        # Save comet exp url
+        url_path = get_increased_path(Path(opts.output_path) / "comet_url.txt")
+        with open(url_path, "w") as f:
+            f.write(exp.url)
+        # Save config file
+        opts_path = get_increased_path(Path(opts.output_path) / "opts.yaml")
+        with (opts_path).open("w") as f:
+            yaml.safe_dump(opts.to_dict(), f)
+
+    # -------------------
+    # -----  Train  -----
+    # -------------------
+    trainer = Trainer(opts, comet_exp=exp)
+    trainer.logger.time.start_time = time()
+    trainer.setup()
+    trainer.train()
+
+    # -----------------------------
+    # -----  End of training  -----
+    # -----------------------------
+
+    pprint("Done training")
+
+
+if __name__ == "__main__":
+
+    main()
