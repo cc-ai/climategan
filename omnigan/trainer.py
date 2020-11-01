2b6981e206144ca8a4b28e735638abb7e1de12cc
Alexia Reynaud <r.alexia@hotmail.com>
2020-11-01 12:15:14 -0500

    Apply softmax to seg output

--- omnigan/trainer.py
+++ omnigan/trainer.py
@@ -15,6 +15,7 @@ warnings.simplefilter("ignore", UserWarning)
 
 import torch
 import torchvision.utils as vutils
+import torch.nn.functional as F
 from torch import autograd
 from addict import Dict
 from comet_ml import Experiment
@@ -824,10 +825,11 @@ class Trainer:
                     if batch_domain == "r":
                         # Entropy minimization loss
                         if self.opts.gen.s.use_minent:
+                            softmax_preds = F.softmax(prediction, dim=1)
                             # Direct entropy minimization
                             update_loss = (
                                 self.losses["G"]["tasks"][update_task]["minent"](
-                                    prediction
+                                    softmax_preds
                                 )
                                 * lambdas.G[update_task]["minent"]
                             )
