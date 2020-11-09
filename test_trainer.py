8e145e36598320595a778899124e998c89442eeb
vict0rsch <vsch@pm.me>
2020-11-08 20:57:55 -0500

    add test trainer

---
+++ test_trainer.py
@@ -0,0 +1,23 @@
+from comet_ml import Experiment
+import omnigan
+
+if __name__ == "__main__":
+
+    opts = omnigan.utils.load_opts()
+    opts.data.check_samples = False
+    opts.train.fid.n_images = 5
+    opts.comet.display_size = 5
+    opts.tasks = ["m", "s", "d"]
+    opts.domains = ["r", "s"]
+    opts.data.loaders.num_workers = 4
+    opts.data.loaders.batch_size = 3
+    opts.data.max_samples = 9
+    opts.train.epochs = 2
+
+    trainer = omnigan.trainer.Trainer(opts=opts, comet_exp=None,)
+    trainer.setup()
+    trainer.train()
+
+    trainer = omnigan.trainer.Trainer(opts=opts, comet_exp=Experiment())
+    trainer.setup()
+    trainer.train()
