aab20f167abf618ea05564dced58cf6621df5cce
melisandeteng <melisande.teng@student.ecp.fr>
2020-11-19 17:12:40 -0500

    fix fix

--- omnigan/eval_metrics.py
+++ omnigan/eval_metrics.py
@@ -85,7 +85,7 @@ def mIOU(pred, label):
     Returns:
         float: mIOU, can be nan
     """
-    num_classes = int(label.max().item())
+    num_classes = pred[-3]
 
     pred = torch.argmax(pred, dim=1).squeeze(1)
     present_iou_list = list()
@@ -94,8 +94,10 @@ def mIOU(pred, label):
     # Note: Following for loop goes from 0 to (num_classes-1)
     # and ignore_index is num_classes, thus ignore_index is
     # not considered in computation of IoU.
-
-    for sem_class in range(num_classes + 1):
+    interesting_classes = [
+        range(num_classes) if num_classes > 2 else int(label.max().item())
+    ]
+    for sem_class in interesting_classes:
         pred_inds = pred == sem_class
         target_inds = label == sem_class
         if target_inds.long().sum().item() > 0:
