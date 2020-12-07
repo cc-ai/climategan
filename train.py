37587f7f09232b92597fbd21134c0d45afbfae75
vict0rsch <vsch@pm.me>
2020-12-07 09:34:37 +0100

    switch to ocnfig

--- train.py
+++ train.py
@@ -55,21 +55,22 @@ def main(opts):
     hydra_opts = Dict(OmegaConf.to_container(opts))
     args = hydra_opts.pop("args", None)
 
-    default = None
+    config_path = args.config
+
     if hydra_opts.train.resume:
         out_ = str(env_to_path(hydra_opts.output_path))
-        default = Path(out_) / "opts.yaml"
-        if not default.exists():
-            default = None
+        config_path = Path(out_) / "opts.yaml"
+        if not config_path.exists():
+            config_path = None
             print("WARNING: could not reuse the opts in {}".format(out_))
-    if default is None:
-        default = args.default or Path(__file__).parent / "shared/trainer/defaults.yaml"
+
+    default = args.default or Path(__file__).parent / "shared/trainer/defaults.yaml"
 
     # -----------------------
     # -----  Load opts  -----
     # -----------------------
 
-    opts = load_opts(args.config, default=default, commandline_opts=hydra_opts)
+    opts = load_opts(config_path, default=default, commandline_opts=hydra_opts)
     if args.resume:
         opts.train.resume = True
 
