fe67dc97e14805345d8276a6973b2457539c08df
nicoduchene <naduchene@casa.energy>
2020-12-09 11:07:12 -0500

    Reworking apply_events and added celery wrapper

--- apply_events.py
+++ apply_events.py
@@ -149,6 +149,155 @@ def parse_args():
 
     return parser.parse_args()
 
+def run_inference_from_trainer(trainer, images_path, output_path, time_inference):
+    args = parse_args()
+    print(
+        "• Using args\n\n"
+        + "\n".join(["{:25}: {}".format(k, v) for k, v in vars(args).items()]),
+    )
+
+    batch_size = args.batch_size
+    half = args.half
+    images_paths = Path(images_path).expanduser().resolve()
+    bin_value = args.flood_mask_binarization
+    outdir = (
+        Path(output_path).expanduser().resolve()
+        if args.output_path is not None
+        else None
+    )
+    # resume_path = args.resume_path
+    # time_inference = args.time
+    n_images = args.n_images
+    xla_purge_samples = args.xla_purge_samples
+
+    if outdir is not None:
+        outdir.mkdir(exist_ok=True, parents=True)
+
+    # -------------------------------
+    # -----  Create time store  -----
+    # -------------------------------
+    stores = {}
+    if time_inference:
+        stores = OrderedDict(
+            {
+                "imports": [import_time],
+                "setup": [],
+                "data pre-processing": [],
+                "encode": [],
+                "mask": [],
+                "flood": [],
+                "depth": [],
+                "segmentation": [],
+                "smog": [],
+                "wildfire": [],
+                "all events": [],
+                "numpy": [],
+                "inference on all images": [],
+                "write": [],
+            }
+        )
+
+    # -------------------------------------
+    # -----  Resume Trainer instance  -----
+    # -------------------------------------
+    print("\n• Initializing trainer\n")
+
+    with Timer(store=stores.get("setup", [])):
+
+        if half:
+            trainer.G.half()
+
+    # --------------------------------------------
+    # -----  Read data from input directory  -----
+    # --------------------------------------------
+    print("\n• Reading & Pre-processing Data\n")
+
+    # find all images
+    data_paths = [i for i in images_paths.glob("*") if is_image_file(i)]
+    base_data_paths = data_paths
+    # filter images
+    if 0 < n_images < len(data_paths):
+        data_paths = data_paths[:n_images]
+    # repeat data
+    elif n_images > len(data_paths):
+        repeats = n_images // len(data_paths) + 1
+        data_paths = base_data_paths * repeats
+        data_paths = data_paths[:n_images]
+
+    with Timer(store=stores.get("data pre-processing", [])):
+        # read images to numpy arrays
+        data = [io.imread(str(d)) for d in data_paths]
+        # resize to standard input size 640 x 640
+        data = [resize(d, (640, 640), anti_aliasing=True) for d in data]
+        # normalize to -1:1
+        data = [(normalize(d.astype(np.float32)) - 0.5) * 2 for d in data]
+
+    n_batchs = len(data) // args.batch_size
+    if len(data) % args.batch_size != 0:
+        n_batchs += 1
+
+    print("Found", len(base_data_paths), "images. Inferring on", len(data), "images.")
+
+    # --------------------------------------------
+    # -----  Batch-process images to events  -----
+    # --------------------------------------------
+    print(f"\n• Creating events on {str(trainer.device)}\n")
+
+    all_events = []
+
+    with Timer(store=stores.get("inference on all images", [])):
+        for b in range(n_batchs):
+            print(f"Batch {b + 1}/{n_batchs}", end="\r")
+            images = data[b * batch_size : (b + 1) * batch_size]
+            if not images:
+                continue
+            # concatenate images in a batch batch_size x height x width x 3
+            images = np.stack(images)
+
+            # Retreive numpy events as a dict {event: array}
+            events = trainer.infer_all(
+                images,
+                numpy=True,
+                stores=stores,
+                bin_value=bin_value,
+                half=half,
+                xla=XLA,
+            )
+
+            # store events to write after inference loop
+            all_events.append(events)
+    print()
+
+    # ----------------------------------------------
+    # -----  Write events to output directory  -----
+    # ----------------------------------------------
+    if outdir:
+        print("\n• Writing")
+        with Timer(store=stores.get("write", [])):
+            for b, events in enumerate(all_events):
+                for i in range(len(list(events.values())[0])):
+                    idx = b * batch_size + i
+                    idx = idx % len(base_data_paths)
+                    stem = Path(data_paths[idx]).stem
+                    for event in events:
+                        im_path = outdir / f"{stem}_{event}.png"
+                        im_data = events[event][i]
+                        io.imsave(im_path, im_data)
+
+    # ---------------------------
+    # -----  Print timings  -----
+    # ---------------------------
+    if time_inference:
+        print("\n• Timings\n")
+        print_store(stores, purge=xla_purge_samples)
+
+    if XLA:
+        metrics_dir = Path(__file__).parent / "config" / "metrics"
+        metrics_dir.mkdir(exist_ok=True, parents=True)
+        now = str(datetime.now()).replace(" ", "_")
+        with open(metrics_dir / f"xla_metrics_{now}.txt", "w",) as f:
+            report = met.metrics_report()
+            print(report, file=f)
 
 if __name__ == "__main__":
 
---
+++ inference_task.py
@@ -0,0 +1,35 @@
+# from celery.signals import worker_process_init
+import os
+import logging
+from celery import Celery, Task
+
+from omnigan.trainer import Trainer
+from apply_events import run_inference_from_trainer
+
+app = Celery('inference_task', broker=os.environ.get('CELERY_BROKER_URL', 'amqp://admin:mypass@rabbitmq:5672'))
+
+class OmniGAN(Task):
+
+    def __init__(self):
+        logging.info(f"Initializing the Trainer")
+        device = None
+        # if XLA:
+        #     device = xm.xla_device()
+        resume_path = os.environ.get('RESUME_PATH', "tests-v1/model")
+        self._trainer = Trainer.resume_from_path(
+            resume_path,
+            setup=True,
+            inference=True,
+            new_exp=None,
+            input_shapes=(3, 640, 640),
+            device=device,
+        )
+
+    @property
+    def trainer(self):
+        return self._trainer
+
+@app.task(base=OmniGAN)
+def infer(images=None, output=None):
+    logging.info(f"Starting inference")
+    run_inference_from_trainer(infer.trainer, images_path='tests-v1/images', output_path='tests-v1/output')
