41ee763d04696194fee6ad6480cd36b8b533d077
vict0rsch <vsch@pm.me>
2021-01-13 17:41:59 -0500

    add test

--- test_trainer.py
+++ test_trainer.py
@@ -277,6 +277,13 @@ if __name__ == "__main__":
             "tasks": ["p"],
             "gen.p.diff_aug.use": True,
         },  # 15
+        {
+            "__use_comet": False,
+            "__doc": "MSD DADA",
+            "__verbose": 1,
+            "gen.s.depth_feat_fusion": True,
+            "gen.s.depth_depth_fusion": True,
+        },  # 16
     ]
 
     n_confs = len(test_scenarios)
