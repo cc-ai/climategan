4f22515f58e83fdc44583affdebd8fe83f382fb3
vict0rsch <vsch@pm.me>
2021-01-20 19:20:40 -0500

    add comments and fix filename typo

--- utils_scripts/download_labelbox.py
+++ utils_scripts/download_labelbox.py
@@ -3,25 +3,39 @@ import os
 from pathlib import Path
 
 if __name__ == "__main__":
+    # labelbox json export path
     path = "/Users/victor/Downloads/export-2021-01-20T20_45_24.103Z.json"
+    # where to write the downloaded images
     out = Path("/Users/victor/Downloads/labelbox_test_flood")
+    # create out dir
     out.mkdir(exist_ok=True, parents=True)
 
+    # load export data
     with open(path, "r") as f:
         data = json.load(f)
 
     for i, d in enumerate(data):
+        # find all polygons
         objects = d["Label"]["objects"]
+        # retrieve original image name
         name = d["External ID"]
         stem = Path(name).stem
+        # output dir for current image
         m_out = out / stem[:30]
         m_out.mkdir(exist_ok=True, parents=True)
+
+        # save 1 png per polygon
         for o, obj in enumerate(objects):
             print(f"{i}/{len(data)} : {o}/{len(objects)}")
+
+            # create verbose label -> "cannotflood", "mustflood"
             label = obj["value"].replace("_", "")
+            # unique polygon mask finlename
             m_path = m_out / f"{stem}_{label}_{o}.png"
+            # download address for curl
             uri = obj["instanceURI"]
+            # command to download the image
             command = f'curl {uri} > "{str(m_path)}"'
-            print(command)
+            # execute command
             os.system(command)
             print("#" * 20)
--- utils_scripts/merge_labelbox_masks.py
+++ utils_scripts/merge_labelbox_masks.py
@@ -3,6 +3,7 @@ import numpy as np
 from skimage.io import imread, imsave
 
 if __name__ == "__main__":
+    # output of download_labelbox.py
     base_dir = Path("/Users/victor/Downloads/labelbox_test_flood")
     assert base_dir.exists()
 
@@ -14,13 +15,22 @@ if __name__ == "__main__":
 
     for k, sd in enumerate(sub_dirs):
         print(k + 1, "/", len(sub_dirs), sd.name)
+
+        # must-flood binary mask
         must = np.stack([imread(i)[:, :, :3] for i in sd.glob("*must*.png")]).sum(0) > 0
+        # cannot-flood binary mask
         cannot = (
             np.stack([imread(i)[:, :, :3] for i in sd.glob("*cannot*.png")]).sum(0) > 0
         )
-        stem = "_".join(list(sd.glob("*must*.png"))[0].stem.split("_")[:-2])
+        # must is red
         must = (must * [0, 0, 255]).astype(np.uint8)
+        # connot is blue
         cannot = (cannot * [255, 0, 0]).astype(np.uint8)
+        # merged labels
         label = must + cannot
+        # check no overlap
         assert sorted(np.unique(label)) == [0, 255]
+        # create filename
+        stem = "_".join(list(sd.glob("*must*.png"))[0].stem.split("_")[:-2])
+        # save label
         imsave(sd / f"{stem}_labeled.png", label)
