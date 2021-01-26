01a4d31b47f5cb0873a11954e455b2da918a9334
vict0rsch <vsch@pm.me>
2021-01-26 15:23:44 -0500

    add upload script

---
+++ utils_scripts/upload_images_to_comet.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import sys
+from argparse import ArgumentParser
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from omnigan.utils import upload_images_to_exp
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--images_path", type="str", default=".")
+    parser.add_argument("-p", "--project_name", type=str, default="omnigan-eval")
+    args = parser.parse_args()
+
+    exp = upload_images_to_exp(
+        Path(args.images_path).resolve(), exp=None, project_name=args.project_name
+    )
+
+    exp.end()
