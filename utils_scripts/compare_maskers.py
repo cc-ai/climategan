d866d29cb2b4b386262f77bfbb39f1ca5310d63c
vict0rsch <vsch@pm.me>
2021-03-05 16:14:43 -0500

    enumerate not items

--- utils_scripts/compare_maskers.py
+++ utils_scripts/compare_maskers.py
@@ -193,7 +193,7 @@ def get_or_load_inferences(
 
 def numpify(outputs):
     nps = []
-    for k, o in outputs.items():
+    for k, o in enumerate(outputs):
         print(o, end=" | ")
         x = (o["x"][0].permute(1, 2, 0).numpy() + 1) / 2
         m = o["m"]
